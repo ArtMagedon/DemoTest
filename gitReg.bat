d3c62c6810ce7e073c86d39b49204718ccb2e152
ArtMagedon <ap20041026@yandex.ru>
2024-05-15 15:25:23 +0300

    first commit

---
+++ gitReg.bat
@@ -0,0 +1,20 @@
+#!/bin/bash
+name="ArtMagedon"					#input git name
+email="ap20041026@yandex.ru"				#input git email
+directory="DemoTest"
+git config --global user.name $name
+git config --global user.email $email
+git config --global init.defaultBranch main
+#git config --list
+mkdir $directory
+cp gitReg.bat $directory"/gitReg.bat"
+cp installConfigGitSsh.bat $directory"/installConfigGitSsh.bat"
+cd $directory
+touch "README.md"
+echo "Created with "$name > README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ArtMagedon/DemoTest.git
+git push -u origin main
