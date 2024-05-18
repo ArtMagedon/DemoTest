5d375f4774e9182f4f942ac88152dfe2062b3e47
ArtMagedon <ap20041026@yandex.ru>
2024-05-18 22:40:48 +0300

    redact push and pull

--- pull.bat
+++ pull.bat
@@ -1,2 +1,44 @@
-cd ~/Data
-git pull git@github.com:ArtMagedon/DemoTest.git
+#!/bin/bash
+if [ -s ~/configGit.dt ]
+then
+
+count=1
+while read line
+do
+#echo "Line $count: $line"
+if [ $count -eq 1 ]
+then
+name=$line
+fi
+if [ $count -eq 2 ]
+then
+email=$line
+fi
+if [ $count -eq 3 ]
+then
+directory=$line
+fi
+if [ $count -eq 4 ]
+then
+link=$line
+fi
+count=$(( $count + 1 ))
+done <<<$(cat ~/configGit.dt)
+
+else
+
+#name1="ArtMagedon"
+#echo "Input name"
+#read name                                      #input git name
+#email1="ap20041026@yandex.ru"
+#echo "Input email"
+#read email                             #input git email
+echo "Input local directory"
+read directory
+echo "Input git directory"
+read link                              #input git directory link
+
+fi
+
+cd ~/$directory
+git pull $link
--- push.bat
+++ push.bat
@@ -1,5 +1,50 @@
+#!/bin/bash
+if [ -s ~/configGit.dt ]
+then
+
+count=1
+while read line
+do
+#echo "Line $count: $line"
+if [ $count -eq 1 ]
+then
+name=$line
+fi
+if [ $count -eq 2 ]
+then
+email=$line
+fi
+if [ $count -eq 3 ]
+then
+directory=$line
+fi
+if [ $count -eq 4 ]
+then
+link=$line
+fi
+count=$(( $count + 1 ))
+done <<<$(cat ~/configGit.dt)
+
+else
+
+#name1="ArtMagedon"
+#echo "Input name"
+#read name                                      #input git name
+#email1="ap20041026@yandex.ru"
+#echo "Input email"
+#read email                             #input git email
+echo "Input local directory"
+read directory
+#echo "Input git directory"
+#read link                              #input git directory link
+
+fi
+
+
+
+
 read commit
-cd ~/Data/
+cd ~/$directory
 #git rm .
 git add .
 git commit -m "$commit"
